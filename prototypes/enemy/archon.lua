7a7bccc789038d55b4bdc15e171d38ccb094aa47
heyqule <hq.jfax@gmail.com>
2021-11-04 22:14:46 -0400

    Flyer units are not-flammable.

--- prototypes/enemy/archon.lua
+++ prototypes/enemy/archon.lua
@@ -75,7 +75,7 @@ function ErmToss.make_archon(level)
             localised_name = { 'entity-name.' .. MOD_NAME .. '/' .. name, level },
             icon = "__erm_toss__/graphics/entity/icons/units/" .. name .. ".png",
             icon_size = 64,
-            flags = { "placeable-enemy", "placeable-player", "placeable-off-grid" },
+            flags = { "placeable-enemy", "placeable-player", "placeable-off-grid", "not-flammable" },
             has_belt_immunity = false,
             max_health = ERM_UnitHelper.get_health(hitpoint, hitpoint * max_hitpoint_multiplier, health_multiplier, level),
             order = MOD_NAME .. '/'  .. name .. '/' .. level,
