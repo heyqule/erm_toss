231889baa98b3f054d2af94182dd998a45e8461a
heyqule <hq.jfax@gmail.com>
2022-07-10 01:18:48 -0400

    - Improve unit performance by changing enemies distraction_cooldown from 20 to 300.

--- prototypes/enemy/templar.lua
+++ prototypes/enemy/templar.lua
@@ -56,7 +56,7 @@ local incremental_movement_speed = 0.05
 local vision_distance = 30
 
 local pollution_to_join_attack = 250
-local distraction_cooldown = 20
+local distraction_cooldown = 300
 
 -- Animation Settings
 local unit_scale = 1.3
