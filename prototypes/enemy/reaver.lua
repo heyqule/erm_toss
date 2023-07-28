c345e162eea340463498dec8c9d68b1a34c92eaf
heyqule <hq.jfax@gmail.com>
2023-07-28 11:12:48 -0400

    1.20.1 push

--- prototypes/enemy/reaver.lua
+++ prototypes/enemy/reaver.lua
@@ -51,8 +51,8 @@ local incremental_attack_speed = 280
 local attack_range = ERM_Config.get_max_attack_range()
 
 
-local base_movement_speed = 0.15
-local incremental_movement_speed = 0.125
+local base_movement_speed = 0.125
+local incremental_movement_speed = 0.1
 
 -- Misc Settings
 local vision_distance = ERM_UnitHelper.get_vision_distance(attack_range)
