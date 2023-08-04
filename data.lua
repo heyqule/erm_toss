fafd1a30bfdcaf6107b7e3c1286b870cc5200ce2
heyqule <hq.jfax@gmail.com>
2023-08-04 01:57:17 -0400

    Autoplace refactor

--- data.lua
+++ data.lua
@@ -15,6 +15,17 @@ data:extend(
     }
 })
 
+-- This set of data is used for set up default autoplace calculation.
+data.erm_enemy_races = data.erm_enemy_races or {}
+table.insert(data.erm_enemy_races, {
+    name=MOD_NAME,
+    force=FORCE_NAME,
+    moisture=1, -- 1 = Wet and 2 = Dry
+    aux=1, -- -- 1 = red desert, 2 = sand
+    elevation=3, --1,2,3 (1 low elevation, 2. medium, 3 high elavation)
+    temperature=1, --1,2,3 (1 cold, 2. normal, 3 hot)
+})
+
 require "prototypes.enemy.arbiter"
 require "prototypes.enemy.zealot"
 require "prototypes.enemy.dragoon"
