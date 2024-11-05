874d994dba76f3d6c279d3c9a427c861bb6d86da
heyqule <hq.jfax@gmail.com>
2024-11-04 23:15:52 -0500

    - Unit spawner resistance changes.

--- prototypes/building/robotics_facility.lua
+++ prototypes/building/robotics_facility.lua
@@ -31,19 +31,19 @@ local max_hitpoint_multiplier = settings.startup["enemyracemanager-max-hitpoint-
 
 -- Handles acid and poison resistance
 local base_acid_resistance = 0
-local incremental_acid_resistance = 75
+local incremental_acid_resistance = 35
 -- Handles physical resistance
 local base_physical_resistance = 0
-local incremental_physical_resistance = 85
+local incremental_physical_resistance = 45
 -- Handles fire and explosive resistance
 local base_fire_resistance = 10
-local incremental_fire_resistance = 70
+local incremental_fire_resistance = 30
 -- Handles laser and electric resistance
 local base_electric_resistance = 25
-local incremental_electric_resistance = 55
+local incremental_electric_resistance = 15
 -- Handles cold resistance
 local base_cold_resistance = 10
-local incremental_cold_resistance = 65
+local incremental_cold_resistance = 30
 
 -- Animation Settings
 local unit_scale = 2
